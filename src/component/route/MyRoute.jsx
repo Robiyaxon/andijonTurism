d44a30199589a50b277a0b6ee1890834d245ac7d
Robiyaxon <tuhtaevarobiahon@gmail.com>
2024-04-25 07:49:07 +0500

    fixed

--- src/component/route/MyRoute.jsx
+++ src/component/route/MyRoute.jsx
@@ -26,6 +26,7 @@ import park3 from "../../acsserts/park3.jpg";
 
 import park4 from "../../acsserts/bog1.jpg";
 import park5 from "../../acsserts/bog3.jpg";
+import park6 from "../../acsserts/bog56.jpg";
 
 import main_zoo from "../../acsserts/main_zoo.webp";
 import zoo1 from "../../acsserts/zoo1.jpg";
@@ -67,7 +68,7 @@ const MyRoute = () => {
     {
       id: 2,
       img: park4,
-      title: "'Alisher Navoi' bog'i",
+      title: "Bog`ishamol",
       text2: "Eski shahar",
       text3: "lorem imsone 4556",
       path: "singlepark2"
@@ -254,11 +255,11 @@ const MyRoute = () => {
             title= "Bog`ishamol"
             text= "Andijon viloyati, Bog`ishamol tumani, Bog'ishamol tabiati boyicha mahalliy istirohat bog\`i ko'p yillar mobaynida mamlakatimiz fuqarolarini o'zlarining o'zaro do'stlariga muloqotlashish, o'tkazishlari va dam olgan joylardan dam olgan joylar vaqtlaridan yotkazgan qadimiy va jismoniy madaniy marosimlarini aniqlashlari uchun joylashgan joydir. Bog'ishamol tabiati boyicha istirohat bog'i haqida bir qancha umumiy ma'lumotlar: Bog'ishamol, Andijon viloyati manzilida joylashgan va uning turli xil tabiiy yodgorliklari va madaniy meroslariga ega bo'lgan joydir. Istirohat bog'ida tarkibiy tuzilmalardan foydalanib, mamlakatning milliy madaniyatining eng asosiy qismlarini namoyish etish maqsadida ko'plab tadbirlar o'tkaziladi. Mahalliy madaniyatni, san'atni, muzokaralarini va turizmni rivojlantirish bo'yicha tadbirlar va festival, bayramlar o'tkaziladi. Bog'ishamol tabiati boyicha istirohat bog'i, shahar fuqarolariga va turistlarga tabiatning zo'r mukofotlarini taqdim etadi, shuningdek, yaxshi dam olish va xavfsiz joylashish imkoniyatlarini ham taklif qiladi."    
             img1= {park5}
-            img2= {park1}
-            img3= {park3}
+            img2= {park4}
+            img3= {park6}
             map1= {40.78386437}
             map2= {72.35164365}
-            main_pic= {park5}
+            main_pic= {park4}
             route= "Bog'lar"
             url_image="https://scontent.fura3-1.fna.fbcdn.net/v/t39.30808-6/435786602_122171733230027112_1140284694879207340_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=E8ZkxGYl1VAAb55cIHJ&_nc_ht=scontent.fura3-1.fna&oh=00_AfC72OO79dExQpA6z5Z3FjehMCy0PDwRc0nGJTTI5nPdLg&oe=662BF24F"
             route_path= "park"
